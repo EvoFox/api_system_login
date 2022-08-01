462b261519f89b2afc6067258f57215afecc8245
EvoFox <foulkes.joshua@live.co.uk>
2022-08-01 10:42:50 +0100

    Create company schema, add placeholder controller/routes files

---
+++ src/Models/Company.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const companySchema = new mongoose.Schema({
+    // Store Company Name, this will be checked against when signing up as a user
+	companyName: {
+		type: String,
+		require: true,
+		unique: true,
+    },
+    // Store company email
+	email: {
+		type: String,
+		require: true,
+		unique: true,
+    },
+    // Store company password
+	password: {
+		type: String,
+		require: true,
+		unique: true,
+    },
+    // Store a list of services that they have access to
+	services: [
+		{
+			type: String,
+		},
+	],
+});
