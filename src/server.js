bb25ffefdec303fced27819abe51e90d05df9cb5
EvoFox <foulkes.joshua@live.co.uk>
2022-08-01 16:06:18 +0100

    import and implement routers

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ require("./db/connection.js");
 const express = require("expressjs");
 const cors = require("cors");
 
+// Import Routers
+const companyRouter = require("./Routes/Company.routes");
+const userRouter = require("./Controllers/User.controller");
+
 // Set up express server and port
 const app = express();
 const port = process.env.PORT || 5001;
@@ -12,6 +16,8 @@ app.use(express.json({ limit: "100mb" }));
 app.use(cors);
 
 // Add routers to server
+app.use(companyRouter);
+app.use(userRouter);
 
 // Listen on provided port, or default to 5001
 app.listen(port, () => {
