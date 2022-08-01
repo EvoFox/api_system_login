c29fb6801f6eead9a5ecf0928a9f34cec794bd07
EvoFox <foulkes.joshua@live.co.uk>
2022-08-01 16:04:57 +0100

    write router templates

---
+++ src/Routes/Company.routes.js
@@ -0,0 +1,12 @@
+// Import dependencies
+const { Router } = require("expressjs");
+const {} = require("../Controllers/Company.controller");
+const {} = require("../middleware");
+
+// Create companyRouter
+const companyRouter = Router();
+
+// Define routes
+
+// Export companyRouter
+module.exports = companyRouter;
---
+++ src/Routes/User.routes.js
@@ -0,0 +1,12 @@
+// Import dependencies
+const { Router } = require("expressjs");
+const {} = require("../Controllers/User.controller");
+const { verifyEmail } = require("../middleware");
+
+// Create userRouter
+const userRouter = Router();
+
+// Define routes
+
+// Export userRouter
+module.exports = userRouter;
