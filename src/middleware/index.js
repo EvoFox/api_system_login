41552df4116939b64f61636ae3847e09ee729060
EvoFox <foulkes.joshua@live.co.uk>
2022-08-02 12:22:58 +0100

    Add JSDoc

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -6,11 +6,11 @@ const debug = process.env.DEBUG;
 /**
  * Function will run a RegEx test against an email address to ensure that it is in a valid format,
  * @module Middleware
- * @function
+ * @middleware
  * @throws if req.body.email empty or not valid
- * @param {express.Request} req Express request object
- * @param {express.Response} res Express response object
- * @param {express.NextFunction} next Express next middleware function
+ * @param {Express.Request} req Express request object
+ * @param {Express.Response} res Express response object
+ * @param {Express.NextFunction} next Express next middleware function
  * @return {undefined}
  */
 exports.verifyEmail = async (req, res, next) => {
@@ -50,11 +50,11 @@ exports.verifyEmail = async (req, res, next) => {
 /**
  * Function will encrypt req.body.pass using BCryptJS, utilising the salt from the .env file.
  * @module Middleware
- * @function
+ * @middleware
  * @throws if no parameter is provided to req.body.pass
- * @param {express.Request} req Express Request Object
- * @param {express.Response} res Express Response Object
- * @param {express.NextFunction} next Express Next Middleware Function
+ * @param {Express.Request} req Express Request Object
+ * @param {Express.Response} res Express Response Object
+ * @param {Express.NextFunction} next Express Next Middleware Function
  */
 exports.encryptPassword = async (req, res, next) => {
 	try {
@@ -72,6 +72,32 @@ exports.encryptPassword = async (req, res, next) => {
 		if (debug) {
 			console.log('Error Encrypting Password: ', error);
 		}
+		res.send({
+			success: false,
+			msg: error,
+		});
+	}
+};
+
+/**
+ * Function will add date to request object for storing in database
+ * @module Middleware
+ * @middleware
+ * @param {Express.Request} req Express request object
+ * @param {Express.Response} res Express response object
+ * @param {Express.NextFunction} next Express next function
+ */
+exports.updateLastLogin = (req, res, next) => {
+	try {
+		// Add date to request object for storing in database
+		req.body.lastLogin = Date.now();
+
+		next();
+	} catch (error) {
+		if (debug) {
+			console.log('Error updating last login: ', error);
+		}
+
 		res.send({
 			success: false,
 			msg: error,
