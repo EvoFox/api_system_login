fb0cfcaabb94cd52b7164faf7a72a10cff17a339
EvoFox <foulkes.joshua@live.co.uk>
2022-08-02 12:25:05 +0100

    add routes

--- src/Routes/Company.routes.js
+++ src/Routes/Company.routes.js
@@ -1,12 +1,49 @@
 // Import dependencies
 const { Router } = require('expressjs');
-const {} = require('../Controllers/Company.controller');
-const {} = require('../middleware');
+const {
+	createCompany,
+	loginCompany,
+	updateCompany,
+	deleteCompany,
+} = require('../Controllers/Company.controller');
+const {
+	verifyEmail,
+	encryptPassword,
+	updateLastLogin,
+} = require('../middleware');
 
 // Create companyRouter
 const companyRouter = Router();
 
 // Define routes
+
+// login company after login verifying email, encrypt password and setting last login to current time.
+companyRouter.post(
+	'/login/company',
+	verifyEmail,
+	encryptPassword,
+	updateLastLogin,
+	loginCompany
+);
+
+// create company after verifying email and encrypting password
+companyRouter.post(
+	'/sign-up/company',
+	verifyEmail,
+	encryptPassword,
+	createCompany
+);
+
+// update company after verifying email and encrypting password
+companyRouter.patch(
+	'/update/company',
+	verifyEmail,
+	encryptPassword,
+	updateCompany
+);
+
+// delete company
+companyRouter.delete('/delete/company', deleteCompany);
 
 // Export companyRouter
 module.exports = companyRouter;
