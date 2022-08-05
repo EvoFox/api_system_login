9467fc5ae45ca32ee19f537e731a0fe6d02d2f9a
EvoFox <foulkes.joshua@live.co.uk>
2022-08-05 18:21:05 +0100

    Write userRouter

--- src/Routes/User.routes.js
+++ src/Routes/User.routes.js
@@ -1,12 +1,44 @@
 // Import dependencies
 const { Router } = require('expressjs');
-const {} = require('../Controllers/User.controller');
-const { verifyEmail, encryptPassword } = require('../middleware');
+const {
+	loginUser,
+	createUser,
+	updateUser,
+	removeUser,
+} = require('../Controllers/User.controller');
+
+const {
+	verifyEmail,
+	encryptPassword,
+	updateLastLogin,
+	tokenCheck,
+	comparePass,
+} = require('../middleware');
 
 // Create userRouter
 const userRouter = Router();
 
 // Define routes
+// Login user after verify email, encrypt password and setting last login to current time
+userRouter.post(
+	'/sign-up/user',
+	verifyEmail,
+	encryptPassword,
+	tokenCheck,
+	comparePass,
+	updateLastLogin,
+	createUser
+);
 
+userRouter.post(
+	'/login/user',
+	
+	encryptPassword,
+	updateLastLogin,
+	loginUser
+);
+userRouter.patch('/update/company', tokenCheck, updateUser);
+
+userRouter.delete('/delete/user', removeUser);
 // Export userRouter
 module.exports = userRouter;
