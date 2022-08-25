ee04b0f4f2cbd4a345c69eaf4f47b62efa358859
EvoFox <foulkes.joshua@live.co.uk>
2022-08-25 16:46:39 +0100

    I cannot believe I made this mistake again

--- src/server.js
+++ src/server.js
@@ -1,25 +1,29 @@
 // Import Dependencies
+// Database Connection
 require('./db/connection.js');
+
+// Express Web Server
 const express = require('express');
+// Cross Origin Resource Sharing
 const cors = require('cors');
 
 // Import Routers
-const companyRouter = require('./Routes/Company.routes');
+const { companyRouter } = require('./Routes/Company.routes');
 const userRouter = require('./Routes/User.routes');
 
 // Set up express server and port
 const app = express();
-const port = process.env.PORT || 5001;
+const _PORT = process.env.PORT || 5001;
 
 // Make all requests use JSON and allow external requests to be made.
-app.use(express.json({ limit: '100mb' }));
-app.use(cors);
+app.use(express.json());
+app.use(cors());
 
 // Add routers to server
 app.use(companyRouter);
-app.use(userRouter);
+// app.use(userRouter);
 
 // Listen on provided port, or default to 5001
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
+app.listen(_PORT, () => {
+	console.log(`Listening on port ${_PORT}`);
 });
